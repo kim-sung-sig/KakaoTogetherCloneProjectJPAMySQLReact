366232acf8e897c1d9895fc98cd64158c5578e83
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-06-04 08:36:04 +0900

    FileStorageService.java 업데이트

--- backend/src/main/java/com/example/kakao/global/service/FileStorageService.java
+++ backend/src/main/java/com/example/kakao/global/service/FileStorageService.java
@@ -1,5 +1,50 @@
 package com.example.kakao.global.service.FileStorageService
 
-public class FileStorageSerivce {
+// 이거 내식에 맞게 수정해보자
+@Service
+public class FileStorageService {
 
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(@Value("${file.upload-dir}") String uploadDir) {
+        this.fileStorageLocation = Paths.get(uploadDir)
+            .toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return fileName;
+        } catch (IOException ex) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+        } catch (MalformedURLException ex) {
+            throw new FileNotFoundException("File not found " + fileName, ex);
+        }
+    }
 }
